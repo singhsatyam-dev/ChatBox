eb411fcffea54308e9d334d7d5abb16849627bda
Satyam Kumar Singh <sinhgsatyam1@gmail.com>
2026-02-09 22:44:36 +0530

    User onboarding, online users list, join/leave notifications

--- src/socket/socket.js
+++ src/socket/socket.js
@@ -1,9 +1,36 @@
+//socket id => username
+const users = new Map();
+
 export const socketHandler = (io) => {
   io.on("connection", (socket) => {
-    console.log("User connected", socket.id);
+    console.log("User connected:", socket.id);
 
+    //user join with username
+    socket.on("join", (username) => {
+      users.set(socket.id, username);
+
+      //notification to everyone
+      socket.broadcast.emit("notification", {
+        message: `${username} joined the chat`,
+      });
+
+      //send updated users list
+      io.emit("users:update", Array.from(users.values()));
+    });
+
+    //handle disconnect
     socket.on("disconnect", () => {
-      console.log("User disconnected", socket.id);
+      const username = users.get(socket.id);
+
+      if (username) {
+        users.delete(socket.id);
+
+        socket.broadcast.emit("notification", {
+          message: `${username} left the chat`,
+        });
+        io.emit("users:update", Array.from(users.values()));
+      }
+      console.log("User disconnected:", socket.id);
     });
   });
 };
